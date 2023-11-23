41fd17513b29433848c038416a0fdee1a0aad891
YooJi0 <sopiayoo@naver.com>
2023-11-23 20:25:01 +0900

    dd

---
+++ 1123/regime_asset.py
@@ -0,0 +1,83 @@
+#%%
+# 1. ETF 수익률 가져오기
+# 2. 국면별 날짜 데이터 불러오기
+# 3. 국면별 날짜들의 ETF별 총 수익률 계산하기
+import yfinance as yf
+yf.pdr_override() # 야후에서 데이터를 획득하는 방식이 크롤링으로 변경
+import pandas as pd
+
+# ETF ticker
+ticker = ['SPY', 'LQD', 'IEF']
+# 순서대로 equity(주식), HY(고수익), Emerging market bond, corporate(회사채), Treasury(국채)
+etf_close = pd.DataFrame()
+start = '2002-12-31'
+end = '2021-12-31'
+for i in ticker:
+  etf_close[i] = yf.download(i, start=start, end=end)['Adj Close'] # download 방법
+
+etf_return = etf_close.pct_change().dropna()
+etf_month = etf_return.resample('M').apply(lambda x: (1 + x).prod() - 1)
+etf_month.index = etf_month.index.strftime('%Y-%m')
+#etf_month # = etf_month*100
+
+# 현재는 0.05 상태, *100 하면 5로 바뀜
+
+# 국면별 날짜(월) 데이터 불러오기
+recovery = pd.read_csv('recovery_df.csv')
+expansion = pd.read_csv('expansion_df.csv')
+slowdown = pd.read_csv('slowdown_df.csv')
+contraction = pd.read_csv('contraction_df.csv')
+# 전체 기간 데이터 (Buy&Hold)
+buy_hold = pd.concat([recovery, expansion, slowdown, contraction])
+
+def regime_data(etf_month, regime_df):
+   # 'Date' 열을 datetime 객체로 변환
+    regime_df['Date'] = pd.to_datetime(regime_df['Date'])
+    # etf_month 데이터프레임의 인덱스를 datetime 객체로 변환
+    etf_month.index = pd.to_datetime(etf_month.index)
+    # 공통 날짜를 기준으로 데이터 병합
+    merged_data = pd.merge(regime_df[['Date']], etf_month, left_on='Date', right_on=etf_month.index, how='inner')
+    # 날짜 형식을 'YYYY-MM'으로 변경
+    merged_data['Date'] = merged_data['Date'].dt.strftime('%Y-%m')
+    return merged_data
+
+# 수정된 함수 사용
+etf_BH = regime_data(etf_month, buy_hold)
+etf_recovery = regime_data(etf_month, recovery)
+etf_expansion = regime_data(etf_month, expansion)
+etf_slowdown = regime_data(etf_month, slowdown)
+etf_contraction = regime_data(etf_month, contraction)
+
+def calculate_total_returns(*dfs):
+    # 총 수익률을 저장할 딕셔너리 초기화
+    total_returns = {}
+    columns = ['Recovery', 'Expansion', 'Slowdown', 'Contraction', 'Buy and Hold']
+    etf_name = ['Equity', 'Corporate', 'Treasury']
+
+    # 각 데이터프레임에 대해 총 수익률 계산
+    for df, column in zip(dfs, columns):
+        regime_returns = {}
+        for etf in df.columns[1:]:  # 첫 번째 열(Date)을 제외하고 계산
+            total_return = ((1 + df[etf].mean()) ** 12 - 1) * 100 # percent 단위로 표현함
+            regime_returns[etf] = total_return
+        total_returns[column] = regime_returns 
+
+    # 결과를 데이터프레임으로 변환
+    total_return_df = pd.DataFrame(total_returns)
+    # ETF 이름을 카테고리 이름으로 변경
+    total_return_df.index = etf_name
+    return total_return_df
+
+# 모든 국면별 데이터프레임에 대해 함수 적용
+final_returns = calculate_total_returns(etf_recovery, etf_expansion, etf_slowdown, etf_contraction, etf_BH)
+final_returns = final_returns.round(2).apply(lambda x: x.map(lambda y: f'{y}%'))
+
+# 결과 확인
+print(final_returns)
+
+#%%
+# final_returns 데이터프레임을 CSV 파일로 저장
+final_returns.to_csv('Panel_B.csv', index=True)
+
+
+# %%
---
+++ 1123/risk_premium.py
@@ -0,0 +1,109 @@
+# 해야할 일 Risk Premium을 계산하기
+# 월별 premium을 구하기
+
+# Equity Premium은 단순 수익률이 아니라, Total 수익률을 가지고 계산해야 함
+# Credit Premium은 4가지 케이스에서 회사채에서 3M 국채를 빼는 것으로 단순 수익률을 계산함
+# Term Premium은 duration 별 3가지 케이스에서 단순 수익률을 계산함
+
+
+# Equity Premium
+# S&P500 Total Return Index (SP500) - 10Y US Treasury Total Return (DGS10)
+# S&P500 Total Return Index (SP500) - 10Y Corporate Bond Total Return (HQMCB10YR)
+
+# Credit Premium
+# High Yield (BAMLH0A0HYM2EY) - 3M T-bill (DGS3MO)
+# Leverage Loan (BOGZ1FL623069503Q) - 3M T-bill (DGS3MO) -> 안 쓰기로 함
+# EM US Dollar Index (DTWEXEMEGS) - 3M T-bill (DGS3MO)
+# Investment Grade (WAAA) - 3M T-bill(DGS3MO)
+
+# Term Premium
+# 3Y US Treasury (DGS3)
+# 7Y US Treasury (DGS7)
+# 10Y US Treasury (DGS10)
+
+#%%
+from pandas_datareader import data as web
+import datetime
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 원래 데이터가 percent이고, 데이터를 월별 수익률로 만들어주는 함수
+def percent_M_data(ticker, start, end):
+    data = web.DataReader(ticker, 'fred', start, end).dropna()
+    data = data.resample('M').mean() / 100
+    data.index = data.index.strftime('%Y-%m')
+    return data
+
+# 원래 데이터가 index이고, 데이터를 월별 수익률로 만들어주는 함수
+def index_M_data(ticker, start, end):
+    data = web.DataReader(ticker, 'fred', start, end).dropna()
+    # 월의 첫날과 마지막날의 값을 사용하여 수익률 계산
+    data_monthly_start = data.resample('M').first()
+    data_monthly_end = data.resample('M').last()
+    data = (data_monthly_end - data_monthly_start) / data_monthly_start
+    data.index = data.index.strftime('%Y-%m')
+    return data
+
+
+# 인덱스가 다른 데이터 간의 수익률을 빼는 연산 함수
+def minus(df1, df2):
+    common_index = df1.index.intersection(df2.index)
+    df1_common = df1.loc[common_index].iloc[:, 0]  # 첫 번째 열만 선택
+    df2_common = df2.loc[common_index].iloc[:, 0]  # 첫 번째 열만 선택
+    result = pd.DataFrame(df1_common - df2_common)
+    result.rename(columns={0: 'Return'}, inplace=True)
+    return result
+
+# 설정된 시작일과 종료일
+start = datetime.datetime(2002, 12, 31)
+end = datetime.datetime(2021, 12, 31)
+
+# S&P 500 지수의 월별 데이터 가져오기
+sp500_monthly_data = yf.download('^GSPC', start='2002-12-31', end='2021-12-31', interval='1mo')
+sp500_adj_close = sp500_monthly_data['Adj Close']
+sp500_returns = sp500_adj_close.pct_change().dropna()
+sp500_returns = sp500_returns * 12
+SP500 = pd.DataFrame({'DATE': sp500_returns.index, 'S&P 500': sp500_returns.values})
+SP500.set_index("DATE", inplace=True)
+SP500.index = SP500.index.strftime("%Y-%m")
+
+
+# 필요한 데이터를 FRED에서 가져오기
+# Equity Premium에 필요한 데이터
+#SP500 = index_M_data('SP500', start, end) # 2013-11부터 있음
+US_10Y = percent_M_data('DGS10', start, end) # 1993-01도 있음
+Corp_10Y = web.DataReader('HQMCB10YR', 'fred', start, end).dropna()
+Corp_10Y.index = Corp_10Y.index.strftime('%Y-%m') # 1993-01도 있음
+
+Eq_1 = minus(SP500, US_10Y)
+Eq_2 = minus(SP500, Corp_10Y)
+
+# Credit Premium에 필요한 데이터
+HY = percent_M_data('BAMLH0A0HYM2EY', start, end) #1996-12부터 있음
+EM = index_M_data('DTWEXEMEGS', start, end) #2006-01부터 있음
+IG = percent_M_data('WAAA', start, end) # 1993-01도 있음
+US_3M = percent_M_data('DGS3MO', start, end) # 1993-01도 있음
+
+Credit_1 = minus(HY, US_3M)
+Credit_2 = minus(EM, US_3M)
+Credit_3 = minus(IG, US_3M)
+
+# Term Premium에 필요한 데이터
+US_3Y = percent_M_data('DGS3', start, end) # 1993-01도 있음
+US_7Y = percent_M_data('DGS7', start, end) # 1993-01도 있음
+US_10Y = percent_M_data('DGS10', start, end) # 1993-01도 있음
+
+Term_1 = minus(US_3Y, US_3M)
+Term_2 = minus(US_7Y, US_3M)
+Term_3 = minus(US_10Y, US_3M)
+
+# csv파일로 저장
+Eq_1.to_csv('Eq_1.csv')
+Eq_2.to_csv('Eq_2.csv')
+Credit_1.to_csv('Credit_1.csv')
+Credit_2.to_csv('Credit_2.csv')
+Credit_3.to_csv('Credit_3.csv')
+Term_1.to_csv('Term_1.csv')
+Term_2.to_csv('Term_2.csv')
+Term_3.to_csv('Term_3.csv')
